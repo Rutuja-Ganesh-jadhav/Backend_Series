acb727eef02ac0ac8bdb80d1759b5142cd525ff3
Rutuja Ganesh Jadhav <rutujadhav2503@gmail.com>
2025-11-01 18:32:46 +0530

    utils code discussion

---
+++ src/utils/asynHandler.js
@@ -0,0 +1,28 @@
+// 1st way using promises
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+
+
+
+export {asyncHandler}
+// this is thing happen in last asynchandler function
+// const asyncHandler = () => {} 
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+    // 2nd way using try catch
+// const asyncHandler = (fn) => async(req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res,status(error.code || 500).json({
+//             success:false,
+//             message: err.message
+//         })
+//     }
+// }
